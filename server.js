6d901ef0c6ec543572506a8f3447c09b091bbdae
nguyen.nhat.thanh <nguyen.nhat.thanh@valuence.inc>
2022-05-16 10:10:09 +0700

    init repo

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT;
+
+app.use(cors());
+
+app.get('/users', (req, res) => {
+  res.status(200).json({
+    status: true,
+    data: [
+      {
+        id: 1,
+        name: 'son',
+      },
+      {
+        id: 2,
+        name: 'tung',
+      },
+      {
+        id: 3,
+        name: 'canh',
+      },
+    ],
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listing port: ${port}`);
+});
